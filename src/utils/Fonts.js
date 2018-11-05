096863103c68a82eb936ef4a7eb25f3dbe95d747
Ritesh Nirkhe <ritesh.nirkhe@theaxontech.com>
2018-11-05 13:07:35 +0530

    next commit

---
+++ src/utils/Fonts.js
@@ -0,0 +1,7 @@
+export const Fonts = {
+    SpicyRice: 'SpicyRice-Regular',
+    RobotoItalic: 'Roboto-Italic',
+    RobotoMedium: 'Roboto-Medium',
+    RobotoRegular: 'Roboto-Regular',
+    MontserratSemiBold: 'Montserrat-SemiBold'
+}
